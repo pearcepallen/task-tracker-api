3705031079343c89941f89ac0ba3e030607b0bef
Pearce Allen <pearce.p.allen@gmail.com>
2021-02-19 17:57:35 -0500

    Added tasks model and get api

--- task_tracker/models.py
+++ task_tracker/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    text = models.TextField(blank=True)
+    day = models.TextField(blank=True)
+    reminder = models.BooleanField(default=False)
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "day": self.day,
+            "reminder": self.reminder
+        }
--- task_tracker/views.py
+++ task_tracker/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from .models import *
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world!")
+
+def tasks(request):
+    tasks = Task.objects.all()
+    return JsonResponse([task.serialize() for task in tasks], safe=False)
