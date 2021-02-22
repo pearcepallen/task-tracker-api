59a66d35b1a0bd1980a2ff2d72ac17f52f73abea
Pearce Allen <pearce.p.allen@gmail.com>
2021-02-21 20:48:33 -0500

    Added new task and reminder toggle API

--- task_tracker/urls.py
+++ task_tracker/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("tasks", views.tasks, name="tasks")
+    path("tasks", views.tasks, name="tasks"),
+    path("new", views.new_task, name="new"),
+    path("reminder/<str:task_id>", views.reminder, name="reminder")
 ]
--- task_tracker/views.py
+++ task_tracker/views.py
@@ -1,5 +1,7 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import *
 # Create your views here.
 
@@ -9,3 +11,27 @@ def index(request):
 def tasks(request):
     tasks = Task.objects.all()
     return JsonResponse([task.serialize() for task in tasks], safe=False)
+
+@csrf_exempt
+def new_task(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required"}, status=400)
+
+    data = json.loads(request.body)
+    text = data.get("text", "")
+    day = data.get("day", "")
+    reminder = data.get("reminder", "")
+
+    Task(text=text, day=day, reminder=reminder).save()
+    return JsonResponse({"message": "Task was added successfully"}, status=201)
+
+@csrf_exempt
+def reminder(request, task_id):
+    if request.method != "PUT":
+        return JsonResponse({"error": "PUT request required"}, status=400)
+    
+    task = Task.objects.get(id=task_id)
+    data = json.loads(request.body)
+    task.reminder = data.get("reminder", "")
+    task.save()
+    return HttpResponse(status=204)
